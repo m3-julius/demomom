cd7fa72096e05985cd242b8e3629743547887ba1
Julius Lin <julius.lin@maventree.sg>
2021-06-03 11:13:23 +0800

    Added missing Maven wrapper and a few clean up.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,15 +3,11 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.dao.impl.MOMDAOImpl;
-
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-//		MOMDAOImpl momDao = new MOMDAOImpl();
-//		momDao.getTestTable();
 	}
 
 }
